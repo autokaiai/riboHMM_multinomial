e9401717b25792ac0a34d430de16423ea4209050
autokaiai <kai@autok.ai>
2025-08-03 20:21:48 +0200

    docs: update license copyright

--- validate.py
+++ validate.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Kai Alois Wöllstein
+# Copyright 2025 Kai Wöllstein, Marcel Schulz, Christina Kalk
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
